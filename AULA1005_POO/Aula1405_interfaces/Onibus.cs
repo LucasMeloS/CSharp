1034b45381d968928829a63bd244a102a4fbc129
Lucas <lu_lucasmelo@hotmail.com>
2018-05-14 09:21:49 -0300

    Criando classe onibus

---
+++ AULA1005_POO/Aula1405_interfaces/Onibus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1405_interfaces
+{
+    class Onibus : IMeioTransporte
+    {
+        public int Velocidade { get; set; }
+
+
+        public Onibus()
+        {
+
+            Velocidade = 0;
+
+        }
+
+
+        public void Acelerar()
+        {
+            Velocidade += 5;
+        }
+
+        public void Desacelerar()
+        {
+            Velocidade -= 5; ;
+        }
+    }
+}
+
+
+
--- AULA1005_POO/Aula1405_interfaces/Program.cs
+++ AULA1005_POO/Aula1405_interfaces/Program.cs
@@ -12,12 +12,18 @@ namespace Aula1405_interfaces
         {
 
             Carro car = new Carro();
+            Onibus oni = new Onibus();
 
             Console.WriteLine("Velocidade inicial:" + car.Velocidade);
             car.Acelerar();
             car.Acelerar();
             car.Desacelerar();
             Console.WriteLine("Velocidade Atual:" + car.Velocidade);
+            Console.WriteLine("Velocidade inicial do bus:" + oni.Velocidade);
+            oni.Acelerar();
+            oni.Acelerar();
+            oni.Desacelerar();
+            Console.WriteLine("Velocidade Atual do bus:" + oni.Velocidade);
 
             Console.ReadKey();
 
